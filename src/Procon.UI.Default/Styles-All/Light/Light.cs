2e0452245c2f7e285764bfee0fc179c20e3f5ab4
Cameron Gunnin <Cameron.Gunnin@myrcon.com>
2012-07-01 04:20:09 +0900

    -Minor change to the way the Scroll Bar is displayed.

--- src/Procon.UI.Default/Styles-All/Light/Light.cs
+++ src/Procon.UI.Default/Styles-All/Light/Light.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+
 using Procon.Net.Protocols.Objects;
 using Procon.UI.API;
 using Procon.UI.API.ViewModels;

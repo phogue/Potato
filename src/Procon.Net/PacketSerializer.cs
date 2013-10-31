c4933683dda9e2211682e7a0e3b9fc1930aa3473
Geoff Green <geoff.green@myrcon.com>
2013-10-31 21:42:07 +1030

    Added some comments to the base PacketSerializer<P> class

--- src/Procon.Net/PacketSerializer.cs
+++ src/Procon.Net/PacketSerializer.cs
@@ -1,16 +1,32 @@
-﻿using System;
-
-namespace Procon.Net {
+﻿namespace Procon.Net {
     public abstract class PacketSerializer<P> where P : Packet {
         
-        public UInt32 PacketHeaderSize { get; protected set; }
+        /// <summary>
+        /// The minimum packet size requires to be passed into the packet serializer. Anything smaller
+        /// and it the full header of a packet wouldn't be available, therefore we wouldn't know
+        /// how many bytes the full packet is.
+        /// </summary>
+        public uint PacketHeaderSize { get; set; }
 
-        protected PacketSerializer() {
-            this.PacketHeaderSize = 0;
-        }
-
+        /// <summary>
+        /// Deserializes an array of bytes into a Packet of type P
+        /// </summary>
+        /// <param name="packetData">The array to deserialize to a packet. Must be exact length of bytes.</param>
+        /// <returns>A new packet with data extracted from packetDate</returns>
         public abstract P Deserialize(byte[] packetData);
+
+        /// <summary>
+        /// Serializes a packet into an array of bytes to send to the server.
+        /// </summary>
+        /// <param name="packet">The packe to serialize</param>
+        /// <returns>An array of bytes to send to the server.</returns>
         public abstract byte[] Serialize(P packet);
+
+        /// <summary>
+        /// Fetches the full packet size by reading the header of a packet.
+        /// </summary>
+        /// <param name="packetData">The possibly incomplete packet data, or as much data as we have recieved from the server.</param>
+        /// <returns>The total size, in bytes, that is requires for the header + data to be deserialized.</returns>
         public abstract long ReadPacketSize(byte[] packetData);
     }
 }

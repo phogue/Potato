bfdf08d99d2ea6a8767d987f70675acc09ecc52e
Geoff Green <geoff.green@myrcon.com>
2013-10-31 21:28:38 +1030

    Moved currently unused ResolveHostName to it's own Network utility class.

--- src/Procon.Net/Client.cs
+++ src/Procon.Net/Client.cs
@@ -189,28 +189,6 @@ namespace Procon.Net {
         }
 
         /// <summary>
-        /// Resolves a hostname to an ip address
-        /// </summary>
-        /// <param name="hostName">The hostname or ip address</param>
-        /// <returns></returns>
-        public static IPAddress ResolveHostName(string hostName) {
-            IPAddress address = IPAddress.None;
-
-            if (IPAddress.TryParse(hostName, out address) == false) {
-                try {
-                    IPHostEntry hostEntry = Dns.GetHostEntry(hostName);
-
-                    address = hostEntry.AddressList.Length > 0 ? hostEntry.AddressList[0] : IPAddress.None;
-                }
-                catch {
-                    address = IPAddress.None;
-                }
-            }
-
-            return address;
-        }
-
-        /// <summary>
         /// Method is executed prior to a packet being dispatched after receiving. This allows us
         /// to cancel the packet by returning true.
         /// </summary>

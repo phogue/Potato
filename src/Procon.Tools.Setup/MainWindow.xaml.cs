c9340e77311c60a32c28ea3d3383d6e0e699f455
Geoff Green <geoff.green@myrcon.com>
2013-11-11 14:11:03 +1030

    Completed the first steps for a metro styled app

--- src/Procon.Tools.Setup/MainWindow.xaml.cs
+++ src/Procon.Tools.Setup/MainWindow.xaml.cs
@@ -11,12 +11,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Procon.Tools.Setup {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : MetroWindow {
         public MainWindow() {
             InitializeComponent();
         }

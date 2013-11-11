df86ca8b1f5619f20032479495783963ac09f9e4
Geoff Green <geoff.green@myrcon.com>
2013-11-11 13:30:22 +1030

    Initial setup tool project creation

---
+++ src/Procon.Tools.Setup/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Procon.Tools.Setup {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}

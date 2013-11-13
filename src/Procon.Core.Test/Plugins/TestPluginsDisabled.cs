d6ad69570720425ccf2d81f50d819823df67d328
Geoff Green <geoff.green@myrcon.com>
2013-11-13 17:45:16 +1030

    Added tests for commands on a disabled plugin
    Which was pretty much a copy + paste of the enabled tests, but they do
    serve a purpose ensuring a command is not successful on a disabled
    plugin

---
+++ src/Procon.Core.Test/Plugins/TestPluginsDisabled.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Procon.Core.Security;
+
+namespace Procon.Core.Test.Plugins {
+    using Procon.Core.Connections.Plugins;
+    [TestFixture]
+    public class TestPluginsDisabled {
+
+        /// <summary>
+        /// Tests that a plugin can be Disabled if the parameter matches up to an existing plugin
+        /// and the user has permission
+        /// </summary>
+        [Test]
+        public void TestPluginDisable() {
+            SecurityController security = new SecurityController();
+            PluginController plugins = new PluginController() {
+                Security = security
+            }.Execute() as PluginController;
+
+            plugins.Execute(new Command() {
+                Origin = CommandOrigin.Local,
+                CommandType = CommandType.PluginsEnable,
+                Parameters = new List<CommandParameter>() {
+                    new CommandParameter() {
+                        Data = {
+                            Content = new List<String>() {
+                                plugins.Plugins.First().PluginGuid.ToString()
+                            }
+                        }
+                    }
+                }
+            });
+
+            CommandResultArgs result = plugins.Execute(new Command() {
+                Origin = CommandOrigin.Local,
+                CommandType = CommandType.PluginsDisable,
+                Parameters = new List<CommandParameter>() {
+                    new CommandParameter() {
+                        Data = {
+                            Content = new List<String>() {
+                                plugins.Plugins.First().PluginGuid.ToString()
+                            }
+                        }
+                    }
+                }
+            });
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(CommandResultType.Success, result.Status);
+        }
+
+        /// <summary>
+        /// Tests that a plugin will return false if it is already disabled
+        /// </summary>
+        [Test]
+        public void TestPluginDisableAlreadyDisabled() {
+            SecurityController security = new SecurityController();
+            PluginController plugins = new PluginController() {
+                Security = security
+            }.Execute() as PluginController;
+
+            CommandResultArgs result = plugins.Execute(new Command() {
+                Origin = CommandOrigin.Local,
+                CommandType = CommandType.PluginsDisable,
+                Parameters = new List<CommandParameter>() {
+                    new CommandParameter() {
+                        Data = {
+                            Content = new List<String>() {
+                                plugins.Plugins.First().PluginGuid.ToString()
+                            }
+                        }
+                    }
+                }
+            });
+
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual(CommandResultType.Failed, result.Status);
+        }
+
+        /// <summary>
+        /// Tests that a plugin will return a DoesNotExist message if the player guid does not
+        /// match up to any loaded plugin
+        /// </summary>
+        [Test]
+        public void TestPluginDisableDoesNotExist() {
+            SecurityController security = new SecurityController();
+            PluginController plugins = new PluginController() {
+                Security = security
+            }.Execute() as PluginController;
+
+            CommandResultArgs result = plugins.Execute(new Command() {
+                Origin = CommandOrigin.Local,
+                CommandType = CommandType.PluginsDisable,
+                Parameters = new List<CommandParameter>() {
+                    new CommandParameter() {
+                        Data = {
+                            Content = new List<String>() {
+                                Guid.NewGuid().ToString()
+                            }
+                        }
+                    }
+                }
+            });
+
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual(CommandResultType.DoesNotExists, result.Status);
+        }
+        /// <summary>
+        /// Tests that a parameter is validated for a conversion to a GUID
+        /// </summary>
+        [Test]
+        public void TestPluginDisableInvalidParameter() {
+            SecurityController security = new SecurityController();
+            PluginController plugins = new PluginController() {
+                Security = security
+            }.Execute() as PluginController;
+
+            CommandResultArgs result = plugins.Execute(new Command() {
+                Origin = CommandOrigin.Local,
+                CommandType = CommandType.PluginsDisable,
+                Parameters = new List<CommandParameter>() {
+                    new CommandParameter() {
+                        Data = {
+                            Content = new List<String>() {
+                                "This is an invalid GUID"
+                            }
+                        }
+                    }
+                }
+            });
+
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual(CommandResultType.InvalidParameter, result.Status);
+        }
+
+        /// <summary>
+        /// Tests that a plugin cannot be disabled if the user has insufficient permissions to do so.
+        /// </summary>
+        [Test]
+        public void TestPluginDisableInsufficientPermission() {
+            SecurityController security = new SecurityController();
+            PluginController plugins = new PluginController() {
+                Security = security
+            }.Execute() as PluginController;
+
+            CommandResultArgs result = plugins.Execute(new Command() {
+                Origin = CommandOrigin.Remote,
+                Username = "Phogue",
+                CommandType = CommandType.PluginsDisable,
+                Parameters = new List<CommandParameter>() {
+                    new CommandParameter() {
+                        Data = {
+                            Content = new List<String>() {
+                                plugins.Plugins.First().PluginGuid.ToString()
+                            }
+                        }
+                    }
+                }
+            });
+
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual(CommandResultType.InsufficientPermissions, result.Status);
+        }
+
+        /// <summary>
+        /// Tests that disabling a plugin will allow commands within that plugin to be executed.
+        /// </summary>
+        [Test]
+        public void TestPluginDisableCommandSuccessful() {
+            SecurityController security = new SecurityController();
+            PluginController plugins = new PluginController() {
+                Security = security
+            }.Execute() as PluginController;
+
+            // The plugin will be disabled right now.
+            CommandResultArgs result = plugins.Execute(new Command() {
+                Name = "TestPluginsDisabledCommandResult",
+                Username = "Phogue",
+                Parameters = TestHelpers.ObjectListToContentList(new List<Object>() {
+                    "Return Message"
+                })
+            });
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(CommandResultType.Continue, result.Status);
+            Assert.AreEqual("", result.Message);
+        }
+    }
+}

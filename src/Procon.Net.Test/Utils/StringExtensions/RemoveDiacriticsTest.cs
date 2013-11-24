4c76ebe6ce4f831fd2d7fa7f0ef48e41f2663652
Geoff Green <geoff.green@myrcon.com>
2013-11-24 22:14:08 +1030

    Unit test for StringExtensions.RemoveDiacritics

---
+++ src/Procon.Net.Test/Utils/StringExtensions/RemoveDiacriticsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Procon.Net.Utils;
+
+namespace Procon.Net.Test.Utils.StringExtensions {
+    [TestFixture]
+    public class RemoveDiacriticsTest {
+
+        /// <summary>
+        /// Tests that a string with diacritics is replaced.
+        /// </summary>
+        [Test]
+        public void TestRemoveDiacriticsSuccess() {
+            String removed = "Die Brösel eines Käsekuchen verzücken mich.".RemoveDiacritics();
+
+            Assert.AreEqual("Die Brosel eines Kasekuchen verzucken mich.", removed);
+        }
+    }
+}

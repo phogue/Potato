f7565fd3af9fb44ab4940294a07e0f36c3fb3ef5
Geoff Green <geoff.green@myrcon.com>
2013-11-27 15:23:44 +1030

    Added unit tests for Point3d class

---
+++ src/Procon.Net.Test/Protocols/Objects/Point3DTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Procon.Net.Protocols.Objects;
+
+namespace Procon.Net.Test.Protocols.Objects {
+    [TestFixture]
+    public class Point3DTest {
+
+        /// <summary>
+        /// Tests the point is zeroed when using the empty constructor
+        /// </summary>
+        [Test]
+        public void TestPointZeroed() {
+            Point3D point = new Point3D();
+
+            Assert.AreEqual(0, point.X);
+            Assert.AreEqual(0, point.Y);
+            Assert.AreEqual(0, point.Z);
+        }
+
+        /// <summary>
+        /// Tests the point is parsed successfully when strings are passed into the constructor
+        /// </summary>
+        [Test]
+        public void TestPointParsed() {
+            Point3D point = new Point3D("1", "2", "3");
+
+            Assert.AreEqual(1, point.X);
+            Assert.AreEqual(2, point.Y);
+            Assert.AreEqual(3, point.Z);
+        }
+    }
+}

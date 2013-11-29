7c823a8d83ea5b69b68ae3b98a2fb373d836922b
Geoff Green <geoff.green@myrcon.com>
2013-11-30 01:26:35 +1030

    Commenting
    Except a lot of these minor commits now ReSharper is nagging me to
    comment.

--- src/Procon.Core/Connections/Plugins/IRemotePlugin.cs
+++ src/Procon.Core/Connections/Plugins/IRemotePlugin.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
+using Procon.Core.Events;
+using Procon.Net;
 
 namespace Procon.Core.Connections.Plugins {
-    using Procon.Core.Events;
-    using Procon.Net;
-
+    /// <summary>
+    /// Interface for procon to communicate across the AppDomain to the plugin
+    /// </summary>
     public interface IRemotePlugin : IExecutableBase, IDisposable {
         /// <summary>
         /// The Guid of the executing assembly. Used to uniquely identify this plugin. 
@@ -31,10 +33,24 @@ namespace Procon.Core.Connections.Plugins {
         /// </summary>
         IPluginCallback PluginCallback { set; }
 
+        /// <summary>
+        /// Fired whenever an event is passed from the client, to the game layer
+        /// then processed with as an event from the game server. (OnChat, OnKill etc.)
+        /// </summary>
+        /// <param name="e">Description of the game event</param>
         void GameEvent(GameEventArgs e);
 
+        /// <summary>
+        /// Fired whenever an event is fired from the networking layer (packet sent/recv,
+        /// connection state change or any socket errors)
+        /// </summary>
+        /// <param name="e">Description of the client event</param>
         void ClientEvent(ClientEventArgs e);
 
+        /// <summary>
+        /// Fired whenever an event occurs from Procon, but isolated to this particular plugin.
+        /// </summary>
+        /// <param name="e">Description of the generic event</param>
         void GenericEvent(GenericEventArgs e);
     }
 }

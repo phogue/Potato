a629bfd89235495450ea7e06cf771279f1a38666
Geoff Green <geoff.green@myrcon.com>
2013-12-17 18:54:09 +1030

    Corrected gitignore to allow sql serializers
    These two files were left off of the due to a gitignore. Since this was
    about two weeks ago, it shows the activity of others tracking it's
    progress =\

---
+++ src/Procon.Database.Serialization/Serializers/Sql/SerializerSqlLite.cs
@@ -0,0 +1,19 @@
+﻿using Procon.Database.Serialization.Builders;
+
+namespace Procon.Database.Serialization.Serializers.Sql {
+    /// <summary>
+    /// Serializer for SqlLite support.
+    /// </summary>
+    public class SerializerSqlLite : SerializerSql {
+
+        public override ICompiledQuery Compile(IParsedQuery parsed) {
+           
+            return null;
+        }
+
+        public override ISerializer Parse(Method method, IParsedQuery parsed) {
+
+            return this;
+        }
+    }
+}

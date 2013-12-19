a6700ec3129f1af7957dbfd7780d27d9fd285c8b
Geoff Green <geoff.green@myrcon.com>
2013-12-19 17:16:20 +1030

    Added serializable flag to recent modifier addtions

--- src/Procon.Database.Serialization/Builders/Modifiers/IfNotExists.cs
+++ src/Procon.Database.Serialization/Builders/Modifiers/IfNotExists.cs
@@ -1,7 +1,10 @@
-﻿namespace Procon.Database.Serialization.Builders.Modifiers {
+﻿using System;
+
+namespace Procon.Database.Serialization.Builders.Modifiers {
     /// <summary>
     /// Specifies and action should only be taken if the result does not already exist.
     /// </summary>
+    [Serializable]
     public class IfNotExists : Modifier {
     }
 }

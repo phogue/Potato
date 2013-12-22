137bf57ac7d7dbbe94446e0865b6ce486798465e
Geoff Green <geoff.green@myrcon.com>
2013-12-22 12:36:35 +1030

    Normalizes assembly information

--- src/Procon.Core/Properties/AssemblyInfo.cs
+++ src/Procon.Core/Properties/AssemblyInfo.cs
@@ -1,35 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Procon.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Myrcon Pty. Ltd.")]
-[assembly: AssemblyProduct("Procon.Core")]
-[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c9ba6724-9c66-4897-8599-e9ce0df5b338")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]
--- src/Procon.Tools.NetworkConsole/Properties/AssemblyInfo.cs
+++ src/Procon.Tools.NetworkConsole/Properties/AssemblyInfo.cs
@@ -1,35 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Procon.Tools.NetworkConsole")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Myrcon Pty. Ltd.")]
-[assembly: AssemblyProduct("Procon.Tools.NetworkConsole")]
-[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("664b5538-ba47-48ae-a128-3d5f83153208")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]

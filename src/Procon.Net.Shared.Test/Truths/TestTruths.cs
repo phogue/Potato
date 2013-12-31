16eaa97e8d5f70f01c7dd6db68b1bd708c0962c0
Geoff Green <geoff.green@myrcon.com>
2013-12-31 11:03:02 +1030

    #16 Unit test to enforce serialization attribute
    I might just copy this over to a few other namespaces..

---
+++ src/Procon.Net.Shared.Test/Truths/TestTruths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Procon.Net.Shared.Truths;
+
+namespace Procon.Net.Shared.Test.Truths {
+    [TestFixture]
+    public class TestTruths {
+        /// <summary>
+        /// Test that all non-static classes within the Procon.Net.Shared.Truths namespace
+        /// are marked as serializable.
+        /// </summary>
+        [Test]
+        public void TestClassesAreSerializable() {
+            IEnumerable<Type> nonSerializableTypes = typeof(ITruth).Assembly
+                .GetTypes()
+                // Where it's a class
+                .Where(type => type.IsClass == true)
+                // Where it's not static (both are true when static)
+                .Where(type => type.IsAbstract == false && type.IsSealed == false)
+                // Where the type exists in our namespace
+                .Where(type => type.Namespace != null && type.Namespace.StartsWith("Procon.Net.Shared.Truths"))
+                // Where they are not serializable
+                .Where(type => type.IsSerializable == false);
+
+            Assert.IsEmpty(nonSerializableTypes);
+        }
+    }
+}

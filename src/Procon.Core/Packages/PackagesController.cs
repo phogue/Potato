7ec4c168a4b4b549d540c48efa3d94aad718f691
Geoff Green <geoff.green@myrcon.com>
2014-01-02 16:54:12 +1030

    Added NuGet Core to Procon.Core

---
+++ src/Procon.Core/Packages/PackagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Procon.Core.Shared;
+using Procon.Core.Shared.Models;
+
+namespace Procon.Core.Packages {
+    public class PackagesController : CoreController {
+
+        /// <summary>
+        /// A list of repositories we are connected to
+        /// </summary>
+        List<RepositoryModel> Repositories { get; set; }  
+
+        public PackagesController() {
+            this.Repositories = new List<RepositoryModel>();
+
+        }
+    }
+}

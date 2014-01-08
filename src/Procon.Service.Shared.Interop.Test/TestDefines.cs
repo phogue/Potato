570e69878d25281381e6b2ab624c3c22f41be3ca
Geoff Green <geoff.green@myrcon.com>
2014-01-08 15:54:10 +1030

    Added interop tests between service and procon
    Minor test but the fallout from not testing it and some one refactoring
    is pretty big. It's happened before with plugins in Procon 1.x where
    people come into the project, refactor without thought of potential
    fallout.

---
+++ src/Procon.Service.Shared.Interop.Test/TestDefines.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Procon.Core;
+
+namespace Procon.Service.Shared.Interop.Test {
+    [TestFixture]
+    public class TestDefines {
+        /// <summary>
+        /// Ensures the definition correctly matches the type name. Since Service.Shared does
+        /// not depend on Procon.Core we have to late load the assembly. This just ensures
+        /// that the type name matches in case some one refactors the names in 2 years. It happens and it's bad.
+        /// </summary>
+        [Test]
+        public void TestTypeProconCoreInstanceControllerDefinition() {
+            Assert.AreEqual(typeof(InstanceController).FullName, Defines.TypeProconCoreInstanceController);
+        }
+    }
+}

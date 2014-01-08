054cb04774540681043aed9f725c2b34476d5f1e
Geoff Green <geoff.green@myrcon.com>
2014-01-08 17:42:11 +1030

    Coverage for Procon.Service.Shared.Defines

---
+++ src/Procon.Service.Shared.Test/TestDefines/TestPackageContainingPath.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Procon.Service.Shared.Test.TestDefines {
+    [TestFixture]
+    public class TestPackageContainingPath {
+
+        public readonly String PackagesExists100Path = Path.Combine(Defines.PackagesDirectory, "Exists.1.0.0");
+
+        /// <summary>
+        /// Clears out all files in the packages directory and ensures the packages directory is created.
+        /// </summary>
+        [SetUp]
+        public void CleanPackagesDirectory() {
+            Directory.Delete(Defines.PackagesDirectory, true);
+            Directory.CreateDirectory(Defines.PackagesDirectory);
+            Directory.CreateDirectory(PackagesExists100Path);
+        }
+
+        /// <summary>
+        /// Tests a sub directory in the packages directory will correctly find the packages path
+        /// </summary>
+        [Test]
+        public void TestDirectoryExistsOneDeep() {
+            var directory = new DirectoryInfo(Path.Combine(PackagesExists100Path, "SubDirectoryA"));
+
+            directory.Create();
+
+            var path = Defines.PackageContainingPath(directory.FullName);
+
+            Assert.AreEqual(PackagesExists100Path, path.FullName);
+        }
+
+        /// <summary>
+        /// Tests a two deep sub directory in the packages directory will correctly find the packages path
+        /// </summary>
+        [Test]
+        public void TestDirectoryExistsTwoDeep() {
+            var directory = new DirectoryInfo(Path.Combine(PackagesExists100Path, "SubDirectoryA", "SubDirectoryB"));
+
+            directory.Create();
+
+            var path = Defines.PackageContainingPath(directory.FullName);
+
+            Assert.AreEqual(PackagesExists100Path, path.FullName);
+        }
+
+
+        /// <summary>
+        /// Tests a file in the package root path will be found
+        /// </summary>
+        [Test]
+        public void TestFileExistsRoot() {
+            var file = new FileInfo(Path.Combine(PackagesExists100Path, "A.txt"));
+
+            file.Create().Close();
+
+            var path = Defines.PackageContainingPath(file.FullName);
+
+            Assert.AreEqual(PackagesExists100Path, path.FullName);
+        }
+
+        /// <summary>
+        /// Tests a file in a sub directory in the packages directory will correctly find the packages path
+        /// </summary>
+        [Test]
+        public void TestFileExistsOneDeep() {
+            var file = new FileInfo(Path.Combine(PackagesExists100Path, "SubDirectoryA", "A.txt"));
+
+            file.Directory.Create();
+            file.Create().Close();
+
+            var path = Defines.PackageContainingPath(file.FullName);
+
+            Assert.AreEqual(PackagesExists100Path, path.FullName);
+        }
+
+        /// <summary>
+        /// Tests a file in a two-deep sub directory in the packages directory will correctly find the packages path
+        /// </summary>
+        [Test]
+        public void TestFileExistsTwoDeep() {
+            var file = new FileInfo(Path.Combine(PackagesExists100Path, "SubDirectoryA", "SubDirectoryB", "A.txt"));
+
+            file.Directory.Create();
+            file.Create().Close();
+
+            var path = Defines.PackageContainingPath(file.FullName);
+
+            Assert.AreEqual(PackagesExists100Path, path.FullName);
+        }
+
+        /// <summary>
+        /// Tests that a path that does not exist will travel up to find the base directory.
+        /// </summary>
+        [Test]
+        public void TestDoesNotExistStopAtBaseDirectory() {
+            var path = Defines.PackageContainingPath("does-not-exist");
+
+            Assert.AreEqual(Defines.BaseDirectory, path.FullName);
+        }
+    }
+}
---
+++ src/Procon.Service.Shared.Test/TestDefines/TestPackageVersionDirectory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Procon.Service.Shared.Test.TestDefines {
+    [TestFixture]
+    public class TestPackageVersionDirectory {
+        /// <summary>
+        /// Clears out all files in the packages directory and ensures the packages directory is created.
+        /// </summary>
+        [SetUp]
+        public void CleanPackagesDirectory() {
+            Directory.Delete(Defines.PackagesDirectory, true);
+            Directory.CreateDirectory(Defines.PackagesDirectory);
+        }
+
+        /// <summary>
+        /// Tests that a package directory will not be found if it does not exist.
+        /// </summary>
+        [Test]
+        public void PackageDirectoryDoesNotExist() {
+            String path = Defines.PackageVersionDirectory(Defines.PackagesDirectory, "does-not-exist");
+
+            Assert.IsNull(path);
+        }
+
+        /// <summary>
+        /// Tests that a package directory can be found
+        /// </summary>
+        [Test]
+        public void PackageDirectoryExists() {
+            String packagePath = Path.Combine(Defines.PackagesDirectory, "Exists.1.0.0");
+
+            Directory.CreateDirectory(packagePath);
+
+            String path = Defines.PackageVersionDirectory(Defines.PackagesDirectory, "Exists");
+
+            Assert.AreEqual(packagePath, path);
+        }
+    }
+}

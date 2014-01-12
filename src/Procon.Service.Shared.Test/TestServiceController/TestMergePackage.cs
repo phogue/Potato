45a23b85b15bc64d2094d3f73a4154a490a089bd
Geoff Green <geoff.green@myrcon.com>
2014-01-12 17:41:46 +1030

    Coverage on Merge/Uninstall package

---
+++ src/Procon.Service.Shared.Test/TestServiceController/TestMergePackage.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using Procon.Service.Shared.Test.TestServiceController.Mocks;
+
+namespace Procon.Service.Shared.Test.TestServiceController {
+    [TestFixture]
+    public class TestMergePackage {
+        /// <summary>
+        /// Tests the package dispatched for install if the entry status is set to Stopped
+        /// </summary>
+        [Test]
+        public void TestInstallDispatchedEntryStatusIsStopped() {
+            var merged = false;
+
+            var service = new ServiceController() {
+                Packages = new MockServicePackageManager() {
+                    PackageInstalled = (sender, uri, packageId) => { merged = true; }
+                },
+                Observer = {
+                    Status = ServiceStatusType.Stopped
+                },
+                ServiceLoaderProxyType = typeof(MockServiceLoaderProxy)
+            };
+
+            service.MergePackage("localhost", "id");
+
+            Assert.IsTrue(merged);
+
+            service.Dispose();
+        }
+
+        /// <summary>
+        /// Tests the package dispatched for install if the entry status is set to Started
+        /// </summary>
+        [Test]
+        public void TestInstallDispatchedEntryStatusIsStarted() {
+            var merged = false;
+
+            var service = new ServiceController() {
+                Packages = new MockServicePackageManager() {
+                    PackageInstalled = (sender, uri, packageId) => { merged = true; }
+                },
+                Observer = {
+                    Status = ServiceStatusType.Started
+                },
+                ServiceLoaderProxyType = typeof(MockServiceLoaderProxy)
+            };
+
+            service.MergePackage("localhost", "id");
+
+            Assert.IsTrue(merged);
+
+            service.Dispose();
+        }
+
+        /// <summary>
+        /// Tests that from a stopped state the eventual state is Started
+        /// </summary>
+        [Test]
+        public void TestFromStoppedEndsAsStarted() {
+            var service = new ServiceController() {
+                Packages = new MockServicePackageManager(),
+                Observer = {
+                    Status = ServiceStatusType.Stopped
+                },
+                ServiceLoaderProxyType = typeof(MockServiceLoaderProxy)
+            };
+
+            service.MergePackage("localhost", "id");
+
+            Assert.AreEqual(ServiceStatusType.Started, service.Observer.Status);
+
+            service.Dispose();
+        }
+    }
+}
---
+++ src/Procon.Service.Shared.Test/TestServiceController/TestUninstallPackage.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using Procon.Service.Shared.Test.TestServiceController.Mocks;
+
+namespace Procon.Service.Shared.Test.TestServiceController {
+    [TestFixture]
+    public class TestUninstallPackage {
+        /// <summary>
+        /// Tests the package dispatched for install if the entry status is set to Stopped
+        /// </summary>
+        [Test]
+        public void TestInstallDispatchedEntryStatusIsStopped() {
+            var uninstalled = false;
+
+            var service = new ServiceController() {
+                Packages = new MockServicePackageManager() {
+                    PackageInstalled = (sender, uri, packageId) => { uninstalled = true; }
+                },
+                Observer = {
+                    Status = ServiceStatusType.Stopped
+                },
+                ServiceLoaderProxyType = typeof(MockServiceLoaderProxy)
+            };
+
+            service.UninstallPackage("id");
+
+            Assert.IsTrue(uninstalled);
+
+            service.Dispose();
+        }
+
+        /// <summary>
+        /// Tests the package dispatched for install if the entry status is set to Started
+        /// </summary>
+        [Test]
+        public void TestInstallDispatchedEntryStatusIsStarted() {
+            var uninstalled = false;
+
+            var service = new ServiceController() {
+                Packages = new MockServicePackageManager() {
+                    PackageInstalled = (sender, uri, packageId) => { uninstalled = true; }
+                },
+                Observer = {
+                    Status = ServiceStatusType.Started
+                },
+                ServiceLoaderProxyType = typeof(MockServiceLoaderProxy)
+            };
+
+            service.UninstallPackage("id");
+
+            Assert.IsTrue(uninstalled);
+
+            service.Dispose();
+        }
+
+        /// <summary>
+        /// Tests that from a stopped state the eventual state is Started
+        /// </summary>
+        [Test]
+        public void TestFromStoppedEndsAsStarted() {
+            var service = new ServiceController() {
+                Packages = new MockServicePackageManager(),
+                Observer = {
+                    Status = ServiceStatusType.Stopped
+                },
+                ServiceLoaderProxyType = typeof(MockServiceLoaderProxy)
+            };
+
+            service.UninstallPackage("id");
+
+            Assert.AreEqual(ServiceStatusType.Started, service.Observer.Status);
+
+            service.Dispose();
+        }
+    }
+}

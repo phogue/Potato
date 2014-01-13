5fcda3bea78f76c4c1c5711a263049b0bfea6068
Geoff Green <geoff.green@myrcon.com>
2014-01-13 11:30:35 +1030

    Added nuget package for Myrcon.Protocols.Frostbite

--- src/Procon.Net.Protocols.Myrcon.Frostbite/Properties/AssemblyInfo.cs
+++ src/Procon.Net.Protocols.Myrcon.Frostbite/Properties/AssemblyInfo.cs
@@ -4,7 +4,31 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Procon.Net.Protocols.Frostbite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Procon.Net.Protocols.Myrcon.Frostbite")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Myrcon Pty. Ltd.")]
+[assembly: AssemblyProduct("Procon")]
+[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+
 [assembly: Guid("7737b040-f97c-464b-b576-9194288b5625")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
---
+++ src/Procon.Net.Protocols.Myrcon.Frostbite/README.txt
@@ -0,0 +1,3 @@
+﻿This project will be moved to it's own github repository before the end of Beta.
+
+When it's moved it will then depend on the NuGet packages generated for Procon-2.

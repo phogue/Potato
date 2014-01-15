fd90d53f9fac33587044befb7e8b2f13231a365e
Geoff Green <geoff.green@myrcon.com>
2014-01-15 16:42:43 +1030

    Marked plugin setup assembly as serializable

--- src/Procon.Core.Shared/Plugins/PluginSetup.cs
+++ src/Procon.Core.Shared/Plugins/PluginSetup.cs
@@ -4,6 +4,7 @@ namespace Procon.Core.Shared.Plugins {
     /// <summary>
     /// Handles passing simple variables from Procon AppDomain to the plugin AppDomain
     /// </summary>
+    [Serializable]
     public class PluginSetup : IPluginSetup {
         public String ConnectionGuid { get; set; }
         public String ConfigDirectoryPath { get; set; }

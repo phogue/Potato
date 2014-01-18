2b2cdce13fe2633602f4aac6c0716e17eb038929
Geoff Green <geoff.green@myrcon.com>
2014-01-19 01:43:16 +1030

    #39 Started removing XElement utils

--- src/Procon.Core.Test/Utils/TestXElementExtensions.cs
+++ src/Procon.Core.Test/Utils/TestXElementExtensions.cs
@@ -23,19 +23,5 @@ namespace Procon.Core.Test.Utils {
             Assert.AreEqual("This", elementTester.Word);
             Assert.AreEqual(1, elementTester.Number);
         }
-
-        /// <summary>
-        ///     Tests that a object can be serialized to a XElement object.
-        /// </summary>
-        [Test]
-        public void TestToXElement() {
-            XElement element = new XElementTester() {
-                Word = "This",
-                Number = 1
-            }.ToXElement();
-
-            Assert.AreEqual("This", element.Element("Word").Value);
-            Assert.AreEqual("1", element.Element("Number").Value);
-        }
     }
 }
--- src/Procon.Net.Shared/Utils/XElementExtensions.cs
+++ src/Procon.Net.Shared/Utils/XElementExtensions.cs
@@ -9,12 +9,6 @@ using System.Xml.Serialization;
 
 namespace Procon.Net.Shared.Utils {
     public static class XElementExtensions {
-        public static string ElementValue(this XElement element, string xName) {
-            XElement xNameElement = element.Element(xName);
-
-            return xNameElement != null ? xNameElement.Value : String.Empty;
-        }
-
         /// <summary>
         /// Queries all _Reference_ xml tags with associated Select attributes
         /// </summary>
@@ -49,36 +43,6 @@ namespace Procon.Net.Shared.Utils {
             }
 
             return root;
-        }
-
-        public static XElement ToXElement<T>(this T obj) {
-            using (var memoryStream = new MemoryStream()) {
-                using (TextWriter streamWriter = new StreamWriter(memoryStream)) {
-                    var xmlSerializer = new XmlSerializer(typeof(T));
-
-                    xmlSerializer.Serialize(streamWriter, obj);
-
-                    return XElement.Parse(Encoding.ASCII.GetString(memoryStream.ToArray()));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Converts an object to an XElement then saves it to a textWriter
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="obj"></param>
-        /// <param name="textWriter"></param>
-        public static void WriteXElement<T>(this T obj, TextWriter textWriter) {
-            using (var memoryStream = new MemoryStream()) {
-                using (TextWriter streamWriter = new StreamWriter(memoryStream)) {
-                    var xmlSerializer = new XmlSerializer(typeof(T));
-
-                    xmlSerializer.Serialize(streamWriter, obj);
-                    
-                    XElement.Parse(Encoding.ASCII.GetString(memoryStream.ToArray())).Save(textWriter);
-                }
-            }
         }
 
         public static T FromXElement<T>(this XElement xElement) {

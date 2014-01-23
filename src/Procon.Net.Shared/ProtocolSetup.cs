9e436726ccde51d8ba4d1ee492ca0fe95236b28e
Geoff Green <geoff.green@myrcon.com>
2014-01-24 10:04:56 +1030

    #47 Added protocol setup interface/concrete
    I hate adding interfaces that are just models though.

---
+++ src/Procon.Net.Shared/ProtocolSetup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Procon.Net.Shared {
+    /// <summary>
+    /// Default setup variables used when creating a new protocol
+    /// </summary>
+    public class ProtocolSetup : IProtocolSetup {
+        public String Hostname { get; set; }
+        public ushort Port { get; set; }
+        public String Password { get; set; }
+        public Dictionary<String, String> Variables { get; set; }
+    }
+}

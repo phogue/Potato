095a45892c3d92405acc35c47a125e96274527a6
Geoff Green <geoff.green@myrcon.com>
2014-03-10 14:26:21 +1030

    Moved plugin config test to a more implementation test
    It was almost a double up of another test as-is, but this way it's
    distinct from the other test being a full implementation of saving a
    plugin config enable command

--- src/Procon.Core.Test/Plugins/TestPluginConfig.cs
+++ src/Procon.Core.Test/Plugins/TestPluginConfig.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Procon.Core.Connections;
 using Procon.Core.Connections.Plugins;
 using Procon.Core.Shared;
@@ -9,6 +12,24 @@ using Procon.Core.Shared.Models;
 namespace Procon.Core.Test.Plugins {
     [TestFixture]
     public class TestPluginConfig {
+        /// <summary>
+        /// Test file for us to save/load from.
+        /// </summary>
+        public FileInfo ConfigFile = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configs", "Procon.Core.Test.Plugins.TestPluginConfig.json"));
+
+        [SetUp]
+        public void ClearConfigDirectory() {
+            this.ConfigFile.Refresh();
+
+            if (this.ConfigFile.Directory != null) {
+                if (this.ConfigFile.Directory.Exists == true) {
+                    this.ConfigFile.Directory.Delete(true);
+                }
+
+                this.ConfigFile.Directory.Create();
+            }
+        }
+
         /// <summary>
         /// Tests that an enabled plugin will have the enabled command saved to the config
         /// </summary>
@@ -42,9 +63,18 @@ namespace Procon.Core.Test.Plugins {
 
             plugins.WriteConfig(config);
 
-            Assert.AreEqual(config.Root.First.Value<String>("Name"), "PluginsEnable");
-            Assert.AreEqual(config.Root.First["ScopeModel"].Value<String>("ConnectionGuid"), connectionGuid.ToString());
-            Assert.AreEqual(config.Root.First["ScopeModel"].Value<String>("PluginGuid"), twoPluginGuid.ToString());
+            config.Save(this.ConfigFile);
+
+            // Now load up the config and ensure it saved what we wanted it too.
+
+            JObject deseralized = JsonConvert.DeserializeObject(File.ReadAllText(this.ConfigFile.FullName)) as JObject;
+
+            Assert.IsNotNull(deseralized);
+            Assert.AreEqual("PluginsEnable", deseralized["Procon.Core.Connections.Plugins.CorePluginController"].First.Value<String>("Name"));
+
+
+            Assert.AreEqual(connectionGuid.ToString(), deseralized["Procon.Core.Connections.Plugins.CorePluginController"].First["ScopeModel"].Value<String>("ConnectionGuid"));
+            Assert.AreEqual(twoPluginGuid.ToString(), deseralized["Procon.Core.Connections.Plugins.CorePluginController"].First["ScopeModel"].Value<String>("PluginGuid"));
         }
     }
 }

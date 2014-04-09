8557049217f059026ff6eb19904e37d79d88492e
Geoff Green <geoff.green@myrcon.com>
2014-04-09 23:55:50 +0930

    #37 Added isolation test for protocol sandbox

---
+++ src/Myrcon.Protocols.Test/MockProtocol.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Procon.Net.Shared;
+using Procon.Net.Shared.Actions;
+
+namespace Myrcon.Protocols.Test {
+    /// <summary>
+    /// An empty protocol that should function like aworking protocol (not throw errors at least)
+    /// </summary>
+    [ProtocolDeclaration(Type = "MockProtocol", Name = "Mock Protocol", Provider = "Myrcon")]
+    public class MockProtocol : IProtocol {
+
+        public MockProtocol() : base() {
+            this.State = new ProtocolState();
+            this.Client = new MockClient();
+            this.Options = new ProtocolSetup();
+            this.ProtocolType = new ProtocolType() {
+                Name = "Mock Protocol 3",
+                Type = "MockProtocol",
+                Provider = "Myrcon"
+            };
+        }
+
+        public IClient Client { get; private set; }
+        public IProtocolState State { get; private set; }
+        public IProtocolSetup Options { get; private set; }
+        public string Password { get; set; }
+        public string Additional { get; set; }
+        public IProtocolType ProtocolType { get; private set; }
+        public string ProtocolsConfigDirectory { get; set; }
+        public event Action<IProtocol, IProtocolEventArgs> ProtocolEvent;
+        public event Action<IProtocol, IClientEventArgs> ClientEvent;
+        public IProtocolSetupResult Setup(IProtocolSetup setup) {
+            this.Options = setup;
+            this.Client.Setup(ClientSetup.FromProtocolSetup(setup));
+
+            return new ProtocolSetupResult();
+        }
+
+        public List<IPacket> Action(INetworkAction action) {
+            return null;
+        }
+
+        public IPacket Send(IPacketWrapper packet) {
+            return null;
+        }
+
+        public void AttemptConnection() {
+            
+        }
+
+        public void Shutdown() {
+            
+        }
+
+        public void Synchronize() {
+            
+        }
+    }
+}
---
+++ src/Procon.Core.Test/Protocols/TestProtocolIsolation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using Procon.Core.Protocols;
+using Procon.Core.Shared;
+
+namespace Procon.Core.Test.Protocols {
+    [TestFixture]
+    public class TestProtocolIsolation {
+        [SetUp]
+        public void Initialize() {
+            SharedReferences.Setup();
+        }
+
+        /// <summary>
+        /// Tests that simply loading a protocol does not load the assembly into the current AppDomain.
+        /// </summary>
+        [Test]
+        public void TestIsolationSuccessOnLoad() {
+            var instance = (InstanceController)new InstanceController().Execute();
+
+            var meta = ((ProtocolController)instance.Protocols).Protocols.First(m => m.Name == "Myrcon.Protocols.Test");
+
+            var protocol = meta.ProtocolTypes.First(p => p.Type == "MockProtocol");
+
+            // Now readd the same connection we just added.
+            ICommandResult result = instance.Tunnel(CommandBuilder.InstanceAddConnection(protocol.Provider, protocol.Type, "1.1.1.1", 27516, "password", "").SetOrigin(CommandOrigin.Local));
+
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(CommandResultType.Success, result.CommandResultType);
+
+            // Now make sure our current appdomain is clean of the test plugin
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+                Assert.IsFalse(assembly.FullName.Contains("Myrcon.Protocols.Test"));
+            }
+
+            instance.Dispose();
+        }
+    }
+}
